fe5e77b79c4615404a02ba3cb813576c09f23004
Алексей <info16399@yandex.ru>
2022-06-10 17:39:18 +0300

    Cделал две функции

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
-function getRandom(max, min) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+
+//источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+//Функция возвращающая случайное число
+function getRandom(min, max) {
+  if (min >= max) {
+    throw new Error('Минимальное значение больше или равно максимального');
   }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+  //Функция может не гарантировать верный результат, если в переданном диапазоне нет ни одного подходящего числа???
+
+function checkMaxStringLength(string, maxLength) {
+  if (maxLength >= string.length) {
+    return true
+  }
+  return false
+}
